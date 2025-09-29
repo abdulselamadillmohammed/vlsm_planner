60b280ea849a2a94cf2dbdf24ce5ee6cc530f95c
abdulselamadillmohammed <forcoding39@gmail.com>
2025-09-29 19:06:54 -0400

    more formatting

--- vlsm_planner.rb
+++ vlsm_planner.rb
@@ -6,6 +6,7 @@
 
 # --- Arg orders --- 
 # 0 : subcommands
+# 1 : parent IPv4 network
 # (2n+1) : flags {1,3,5... 2n+1} where n = len(ARGV//2)
 # 2n : arguments
 # -------------------
@@ -24,3 +25,12 @@ puts ("Type of sizes: #{sizes.class}")
 # Here are the sizes: ["500", "200", "200", "50", "20"]
 # Type of sizes: Array
 sizes = sizes.map { |x| x.to_i}
+
+# You can create a hashmap to get 0(1) access but manually 
+# computing upper bounds is relatively negligible since n is 
+# pretty small 
+
+
+# To allow for elegant formatting every sub box should be 
+# 4(3)+ 4 = 16 cause => 255.255.255.255  
+

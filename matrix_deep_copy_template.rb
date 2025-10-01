45a6996354b2e4229d5c19f2363c142ff41fc0d9
abdulselamadillmohammed <forcoding39@gmail.com>
2025-10-01 14:06:38 -0400

    add prepatory arrays for the martix

--- matrix_deep_copy_template.rb
+++ matrix_deep_copy_template.rb
@@ -1,12 +1,19 @@
-original_array = []
+original_martix = []
 
-original_array[0] = ["a"]
-original_array[1] = ["b"]
-original_array[2] = ["c"]
+original_martix[0] = ["a"]
+original_martix[1] = ["b"]
+original_martix[2] = ["c"]
 
-puts "#{original_array}"
+puts "#{original_martix}"
 
+deep_copied_matrix = []
+for _ in 0..(original_martix.length - 1)
+    deep_copied_matrix << []
+end
 
-# for 0..(original_array.length - 1)
-#     original_array <<
-# end
+for i in 0..(original_martix.length - 1)
+    for i in 0..(x.length - 1)
+        
+    end
+end
+puts "#{deep_copied_matrix}"

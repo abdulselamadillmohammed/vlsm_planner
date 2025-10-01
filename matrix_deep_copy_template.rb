861016a94261975e817b359e9b8c65f4b75687b5
abdulselamadillmohammed <forcoding39@gmail.com>
2025-10-01 14:02:39 -0400

    begin implementing matrix deep copying template

---
+++ matrix_deep_copy_template.rb
@@ -0,0 +1,12 @@
+original_array = []
+
+original_array[0] = ["a"]
+original_array[1] = ["b"]
+original_array[2] = ["c"]
+
+puts "#{original_array}"
+
+
+# for 0..(original_array.length - 1)
+#     original_array <<
+# end
